0f3bccfd785173e90d11c098b6682e1aabc12b33
Nasrul <nasrul2001@gmail.com>
2022-08-15 22:10:07 +0530

    First Commit

---
+++ backend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+// Controller Functions
+const { signupUser, loginUser } = require('../controllers/userController')
+
+const router = express.Router()
+
+// Login Route
+router.post('/login', loginUser)
+
+// Signup Route
+router.post('/signup', signupUser)
+
+module.exports = router
